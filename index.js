f74d1bc0d3b0038bac1f102941a3afa35f666a53
logan472 <157203024+logan472@users.noreply.github.com>
2024-05-20 17:04:34 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const Discord = require('discord.js');
 const fetch = require('node-fetch');
 
 let config = {
-    groupId: '',
-    groupIconURL: '',
-    webhookId: '',
-    webhookToken: ''
+    groupId: '2859030',
+    groupIconURL: 'https://tr.rbxcdn.com/452ac2b8d8f942ab882abd965a051882/150/150/Image/Webp',
+    webhookId: '1242009937823924265',
+    webhookToken: 'pueB0Oib-3F5Jr5YDbzkwfln21GM8esogFyOzHRvRjpOY533wXmFtTRJ5iGWj5rVyr-R'
 }
 
 let client = new Discord.WebhookClient(config.webhookId, config.webhookToken);
